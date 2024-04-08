6b2d06448e40245ace9b364f3bc6246704dfabcb
Mariam <kelanimaryam1234@gmail.com>
2024-04-07 19:55:17 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,59 +1,49 @@
 #!/usr/bin/python3
-"""
-Distributes an archive to your web servers,
-using the function do_deploy
-"""
-from os import path
-from time import strftime
-from fabric.api import task
-from fabric.api import local
+# Fabfile to distribute an archive to a web server.
+import os.path
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
-from datetime import datetime
 
-
-env.hosts = ['100.25.13.203', '34.201.61.187']
-env.user = "ubuntu"
-env.key_filename = '~/.ssh/school'
-
-
-def do_pack():
-    """Generates an archive from the content of web_static folder"""
-    filename = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-        return "versions/web_static_{}.tgz".format(filename)
-    except Exception as e:
-        return None
+env.hosts = ["52.91.153.180", "54.164.73.177"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    try:
-        if not os.path.exists(archive_path):
-            return False
-        put(archive_path, '/tmp/')
-        timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'
-            .format(timestamp))
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C '
-            '/data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
-        run('sudo rm /tmp/web_static_{}.tgz'
-            .format(timestamp))
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* '
-            '/data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
-        run('sudo rm -rf /data/web_static/releases/web_static_{}/web_static'
-            .format(timestamp))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s /data/web_static/releases/web_static_{}/ '
-            '/data/web_static/current'
-            .format(timestamp))
-    except Exception as e:
-        print(e)
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
     return True
