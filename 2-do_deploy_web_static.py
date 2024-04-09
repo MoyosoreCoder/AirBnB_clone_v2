ca1ffaa28c4849e7f556d1ceb553612c6189c8cf
Mariam <kelanimaryam1234@gmail.com>
2024-04-08 19:20:12 -0700

    2-do_deploy_web_static.py corrected

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-env.hosts = ["52.91.153.180", "54.164.73.177"]
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['52.91.153.180', '54.164.73.177']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
